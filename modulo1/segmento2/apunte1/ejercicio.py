58396e06288d666ef62957a59b744615d7f73300
Adrián Lucero <alucero@AdrianLucero.alucero>
2018-10-02 22:01:27 -0300

    Primer comit

---
+++ modulo1/segmento2/apunte1/ejercicio.py
@@ -0,0 +1,4 @@
+nombre = 1
+print (nombre)
+nombre = ("German")
+print (nombre)
---
+++ saludador.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+declare nombre=""
+
+read -p "Ingresa tu nombre: " nombre
+
+echo "hola $nombre"
+
+exit 0
